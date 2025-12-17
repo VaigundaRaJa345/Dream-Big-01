9786af827f50cd9db1c20820d2f36b8fe188d899
VaigundaRaJa345 <149135689+VaigundaRaJa345@users.noreply.github.com>
2025-12-17 20:34:14 +0530

    Fix router setup for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded correctly on GitHub Pages
+  base: '/Dream-Big-01/' , // Ensures assets are loaded correctly on GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
