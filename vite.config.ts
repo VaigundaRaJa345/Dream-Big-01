18d5b2cee96a71d505b1fea349b45f9daa95665e
VaigundaRaJa345 <149135689+VaigundaRaJa345@users.noreply.github.com>
2025-12-17 21:17:23 +0530

    Fix workflow location and enable GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Dream-Big-01/',
   plugins: [react()],
-  base: '/Dream-Big-01/' , // Ensures assets are loaded correctly on GitHub Pages
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    sourcemap: true
   }
-});
+})
