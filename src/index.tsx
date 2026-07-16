7ef59a86c54521c455ebc2af3195ffa765ed3886
VaigundaRaJa345 <149135689+VaigundaRaJa345@users.noreply.github.com>
2026-07-17 00:12:43 +0530

    chore: Integrate @vercel/analytics and update vercel.json output directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(rootElement).render(
     <GlobalError>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </GlobalError>
   </React.StrictMode>
